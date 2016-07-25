185a255aa1c40f694e0c8da8c4b3ec1ee3280afe
embg <elliot.gorokhovsky@gmail.com>
2016-07-25 13:52:25 -0600

    Nelder-Mead and Pareto; tested, working

--- pysr.py
+++ pysr.py
@@ -1,4 +1,3 @@
-import copy
 import warnings
 warnings.simplefilter('ignore')
 import scipy
@@ -51,8 +50,6 @@ pset.addPrimitive(lambda x,y: np.nan_to_num(np.divide(x,y)),
                   2, name='div')
 pset.addPrimitive(lambda x,y: np.nan_to_num(np.power(np.abs(x),y)),
                   2, name='pow')
-pset.addPrimitive(lambda x: np.nan_to_num(np.sin(x)),
-                  1, name='sin')
 
 pset.addEphemeralConstant('C',    #95% of the time falls within [-100, 100]
                           lambda: random.gauss(0, 50))
@@ -68,7 +65,6 @@ creator.create('Individual',
 
 #Define our evaluation function
 def optimizeConstants(individual):
-    individual = copy.deepcopy(individual)
     #Optimize the constants
     constants = np.array(list(map(lambda n: n.value,
                               filter(lambda n: isinstance(n, gp.C),individual))))
@@ -86,14 +82,14 @@ def optimizeConstants(individual):
         def evaluate(constants, individual):
             individual = setConstants(individual, constants)
             func = toolbox.lambdify(expr=individual)
-            diff = (func(*X) - y)
-            return np.nan_to_num(diff)
+            diff = (func(*X) - y)**2
+            return np.nan_to_num(np.sum(diff))
 
-        def evaluateLM(constants):
+        def evaluateNM(constants):
             return evaluate(constants, individual)
 
-        res = scipy.optimize.leastsq(evaluateLM, constants)
-        individual = setConstants(individual, res[0])
+        res = scipy.optimize.minimize(evaluateNM, constants, method='Nelder-Mead')
+        individual = setConstants(individual, res.x)
     return individual
     
 def evaluate(individual):
@@ -108,7 +104,7 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.ex
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("lambdify", gp.compile, pset=pset)
 toolbox.register("evaluate", evaluate)
-toolbox.register("select", tools.selTournament, tournsize=4)
+toolbox.register("select", tools.selNSGA2)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genGrow, min_=0, max_=2)
 toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
@@ -175,9 +171,8 @@ def evolve(population, toolbox, popSize, cxpb, mutpb, ngen,
         offspring = varOr(population, toolbox, lambda_, cxpb, mutpb)
 
         #Optimize the new individuals
-        #old = list(map(lambda ind: evaluate(ind)[0], offspring))
         offspring = list(toolbox.map(optimizeConstants, offspring))
-        #print([evaluate(ind)[0]/oldVal for ind,oldVal in zip(offspring, old)])
+        
         # Evaluate the individuals with an invalid fitness
         fitnesses = toolbox.map(toolbox.evaluate, offspring)
 
@@ -189,13 +184,14 @@ def evolve(population, toolbox, popSize, cxpb, mutpb, ngen,
             halloffame.update(offspring)
 
         # Select the next generation population
-        population = toolbox.select(population + offspring, mu)
+        population = (toolbox.select(population + offspring, math.floor(.99*mu)) +
+                      tools.selBest(population + offspring, math.floor(.01*mu)))
                      
         # Pickle the state
         if pickleFile is not None:
             cp = dict(population=population, generation=gen, halloffame=halloffame,
                       logbook=logbook, rndstate=random.getstate())
-            with open(pickleFile+str(gen), "wb") as cp_file:
+            with open(pickleFile, "wb") as cp_file:
                 pickle.dump(cp, cp_file)
 
         # Update the statistics with the new population
@@ -258,7 +254,6 @@ def main():
     sympy_namespace['mul'] = sympy.Mul
     sympy_namespace['div'] = lambda a,b: a*sympy.Pow(b,-1)
     sympy_namespace['pow'] = lambda a,b: sympy.Pow(sympy.Abs(a), b)
-    sympy_namespace['sin'] = sympy.sin
 
     import re
     str_best = re.sub(r'ARG(\d)',r'x_\1',str(best))
